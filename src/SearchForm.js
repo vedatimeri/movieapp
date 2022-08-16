35744419ac3441cf8a65d6ee5656f6f24fbefb02
Vedat <vedat.imeri1@gmail.com>
2022-08-16 14:37:40 +0200

    movie-app

---
+++ src/SearchForm.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+const SearchForm = () => {
+  const { error, query, setQuery } = useGlobalContext();
+
+  return (
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+      <h3>Search movies</h3>
+      <input
+        type="text"
+        className="form-input"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {error.show && <div className="error">{error.msg}</div>}
+    </form>
+  );
+};
+
+export default SearchForm;
